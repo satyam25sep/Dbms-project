a142f5aa0c5f3d8ab739f41359c94f3b05603602
satyam25sep <satyam25sep@github.com>
2018-10-04 21:55:38 +0530

    done

---
+++ Code/edit_profile.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+   <link rel="stylesheet"  href="edit_profile.css">
+</head>
+<body>
+  <div id="sc-edprofile">
+  <h1>Edit Profile Form</h1>
+  <div class="sc-container">
+    <input type="text" placeholder="Username" />
+    <input type="text" placeholder="Email Address" />
+    <input type="password" placeholder="Password" />
+    <input type="text" placeholder="Website" />
+    <input type="text" placeholder="First Name" />
+    <input type="text" placeholder="Last Name" />
+    <input type="text" placeholder="Phone Number" />
+    <textarea placeholder="Bio" /></textarea>
+    <select>
+      <option value="">Male</option>
+      <option value="">Female</option>
+    </select>
+    <input type="text" placeholder="Facebook Profile URL" />
+    <input type="text" placeholder="Twitter Profile URL" />
+    <input type="text" placeholder="LinkedIn Profile URL" />
+    <input type="submit" value="Register" />
+  </div>
+</div>
+  
+
+
+</body>
+</html>
